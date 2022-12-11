ac5973bba1b42e39ef103d67f76cc47d886a0478
Yagapriyan <111879218+Yagapriyan@users.noreply.github.com>
2022-12-11 08:07:39 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,131 @@
+package NewOne;
+/*
+//Fibonacci series with temp
+public class Fibonacci
+{
+	public static void main(String [] args)
+	{
+		int F=0;
+		int S=1;
+		int n=10;
+  	for(int i=0;i<=n;i++)
+		{
+			
+		int T=F+S;
+		    F=S;
+		    S=T;
+		    System.out.println(T);
+		}
+		
+	}
+*/
+
+/*
+//Fibonacci series without temp
+public class Fibonacci
+{
+	public static void main(String [] args)
+	{
+		int F=0;
+		int S=1;
+		int n=10;
+	for(int i=0;i<=n;i++)
+	    {
+		    S=F+S;
+		    F=S-F;
+		    System.out.println(S);
+		}
+		
+	}
+
+} */
+
+
+/*
+//Happy numbers
+public class Fibonacci
+{
+	public static void main (String args[])
+	{
+		int A=23;
+		double B=A%10;
+		double C=A/10;
+		int n=2;
+		do(int i=23;i>=0)
+		{
+	    double D=Math.pow(B, n);
+	    double E=Math.pow(C,n);
+	    double F=D+E;
+	    i++;
+		}
+		while(F=0);
+	System.out.println(F);
+    }
+}*/
+
+
+/*
+	public class Fibonacci
+	{
+		void givenNumber(int n)
+		{
+			int sum=0;
+			while(n>0)
+			{
+				    while(n>0)
+				    {
+				        int a=n%10;
+				        int b=a*a;
+					sum=sum+b;
+					n=n/10;
+				     }
+				    if(sum>9)
+				    {
+				    	n=sum;
+				    	sum=0;
+				    }
+				    else
+				    {
+				    	break;
+				    }
+			 } 
+			if(sum==1)
+			{
+				System.out.print("happy number");
+			}
+			else
+			{
+				System.out.print("not a happy number");
+			}
+		}
+		public static void main(String[] args) 
+		{
+			Fibonacci num = new Fibonacci();
+			num.givenNumber(10);
+		}
+	   }
+	*/
+
+
+
+
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
